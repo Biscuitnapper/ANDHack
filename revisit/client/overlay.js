9517de5b495f6ac831a47b4368ae495aa189726c
Lloyd Henning <lloyd.w.henning@gmail.com>
2015-12-02 20:58:57 +0000

    Fix videos resizing properly on different screens

--- revisit/client/overlay.js
+++ revisit/client/overlay.js
@@ -1,15 +1,65 @@
+Template.VideoOverlay.onRendered(function(){
+  var newVideo = this.find('.new-video');
+  var archiveVideo = $('.archive-video');
+
+  var setTransform = function() {
+
+    var archiveVideoWidth = archiveVideo.width();
+    var archiveVideoHeight = archiveVideo.height();
+
+    // ratio of the current width to native
+    var widthRatio = archiveVideoWidth / 800;
+    // ratio of current height to native
+    var heightRatio = archiveVideoHeight / 450;
+
+    // Figure out which dimension we are constrained by
+    var ratio;
+    if (widthRatio > heightRatio) {
+      ratio = heightRatio;
+    } else {
+      ratio = widthRatio;
+    }
+
+    var actualVideoWidth = 800 * ratio;
+    var actualVideoHeight = 450 * ratio;
+
+    translateX = actualVideoWidth * -0.067;
+    translateY = actualVideoHeight * -0.1;
+
+    Session.set('translateX', translateX);
+    Session.set('translateY', translateY);
+
+  }
+
+  $(window).on('resize', function() {
+    setTransform();
+  });
+
+  // Needs to be initialized.
+  setTransform();
+
+});
+
 Template.VideoOverlay.helpers({
    'opacity': function(){
-      return Session.get("opacity");   
-   }   
+     return Session.get("opacity");
+   },
+
+   'translateX': function() {
+     return Session.get('translateX');
+   },
+
+   'translateY': function() {
+     return Session.get('translateY');
+   }
 });
 
 Template.VideoOverlay.events({
-   
+
    'input #opacity-slider' : function(event){
         event.preventDefault();
-        
+
         Session.set("opacity", $(event.target).val());
-    
+
    }
 });
